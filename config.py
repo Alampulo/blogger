36b361622891adb781ce32caf31258457449010b
Bryan Juma <jumabryan10@gmail.com>
2018-09-17 19:07:57 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/blogs'
+    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST='app/static/photos'
     #email configurations
@@ -16,7 +16,9 @@ class ProdConfig(Config):
         SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/blogs'
     DEBUG = True
+
 
 config_options = {
     'development':DevConfig,
